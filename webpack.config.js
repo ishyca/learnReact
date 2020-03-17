aabeed5487fcc72d421d0439cb448ec51d2051c9
kira wu <767024+ishyca@users.noreply.github.com>
2020-03-17 15:07:15 -0400

    some more webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    mode: "production",
+
+    devtool: "source-map",
+
+    resolve: {
+	extensions: [".ts", ".tsx"]
+    },
+
+    module: {
+	rules: [
+	    {
+		test: /\.ts(x?)$/,
+		exclude: /node_modules/,
+		use: [
+		    {
+			loader: "ts-loader"
+		    }
+		]
+		
+	    },
+	    
+	]
+
+    }
+
+
+
+}
